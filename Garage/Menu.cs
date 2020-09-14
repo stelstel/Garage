4b5e9ebde494b164dad250d18e4932826d0faefe
stelstel <sthlm_32@hotmail.com>
2020-09-14 22:08:01 +0200

    -

--- Garage/Menu.cs
+++ Garage/Menu.cs
@@ -42,44 +42,73 @@ namespace Garage
 
         private static void CreateVehicle()
         {
-            Ui.PrintLine("Input type of vehicle: ");
-            Ui.PrintLine("1: Airplane");
-            Ui.PrintLine("2: Boat");
-            Ui.PrintLine("1: Bus");
-            Ui.PrintLine("1: Car");
-            Ui.PrintLine("1: Motorcycle");
-
-            string type = Ui.GetInput();
-
             string vehicleType = "";
+            bool correctType = false;
 
-            switch (type)
+            do
             {
-                case "1":
-                    vehicleType = "Airplane";
-                    break;
-                case "2":
-                    vehicleType = "Boat";
-                    break;
-                case "3":
-                    vehicleType = "Bus";
-                    break;
-                case "4":
-                    vehicleType = "Car";
-                    break;
-                case "5":
-                    vehicleType = "Motorcycle";
-                    break;
-                default:
-                    break;
-            }
+                Ui.PrintLine("Input type of vehicle: ");
+                Ui.PrintLine("1: Airplane");
+                Ui.PrintLine("2: Boat");
+                Ui.PrintLine("3: Bus");
+                Ui.PrintLine("4: Car");
+                Ui.PrintLine("5: Motorcycle");
 
-            Ui.Print($"Input weight of the {vehicleType}: ");
-            double weight;
-            double.TryParse(Ui.GetInput(), out weight);
+                string type = Ui.GetInput();
+
+                switch (type)
+                {
+                    case "1":
+                        vehicleType = "Airplane";
+                        correctType = true;
+                        break;
+                    case "2":
+                        vehicleType = "Boat";
+                        correctType = true;
+                        break;
+                    case "3":
+                        vehicleType = "Bus";
+                        correctType = true;
+                        break;
+                    case "4":
+                        vehicleType = "Car";
+                        correctType = true;
+                        break;
+                    case "5":
+                        vehicleType = "Motorcycle";
+                        correctType = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (!correctType);        
+
+            bool correctWeight = false;
+
+            do
+            {
+                Ui.Print($"Input weight of the {vehicleType}: ");
+                double weight;
+
+                if (!double.TryParse(Ui.GetInput(), out weight))
+                {
+                    Ui.PrintLine("Incorrect weight. Try again!");
+                }
+                else
+                {
+                    correctWeight = true;
+                }
+            } while (!correctWeight);
+
+            bool correctRegNum = false;
+
+            do
+            {
+                Ui.Print($"Input registration number of the {vehicleType}:");
+                string regNum = Ui.GetInput();
+            } while (!correctRegNum);
+
             
-            Ui.Print($"Input registration number of the {vehicleType}:");
-            string regNum = Ui.GetInput();
             //string registrationNumber, string colour, int numberOfWheels
         }
     }
