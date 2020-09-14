86a9372786e62aa3fd0a5be6763f98bb087619de
stelstel <sthlm_32@hotmail.com>
2020-09-14 20:08:17 +0200

    Added interface UI. Added class ConsoleUI

---
+++ Garage/ConsoleUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    /// <summary>
+    /// Prints to and reads from Console
+    /// </summary>
+    public class ConsoleUI : IUI
+    {
+        /// <summary>
+        /// Reads input from Console
+        /// </summary>
+        /// <returns>User input</returns>
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prints string to Console
+        /// </summary>
+        /// <param name="message">string to print</param>
+        public void Print(string message)
+        {
+            Console.Write(message);
+        }
+
+        /// <summary>
+        /// Prints string to Console ending with line break
+        /// </summary>
+        /// <param name="message">string to print</param>
+        public void PrintLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Empties console
+        /// </summary>
+        public void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ Garage/IUI.cs
@@ -0,0 +1,28 @@
+﻿namespace SkalProj_Datastrukturer_Minne
+{
+    interface IUI
+    {
+        /// <summary>
+        /// Reads input
+        /// </summary>
+        /// <returns>input</returns>
+        string GetInput();
+
+        /// <summary>
+        /// Prints string
+        /// </summary>
+        /// <param name="message">String to print</param>
+        void Print(string message);
+
+        /// <summary>
+        /// Prints string ending with line break
+        /// </summary>
+        /// <param name="message">String to print</param>
+        void PrintLine(string message);
+
+        /// <summary>
+        /// Empties output screen
+        /// </summary>
+        void Clear();
+    }
+}
