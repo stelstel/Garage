3b65f76ca5b379e85e1bb550006b195318040781
stelstel <sthlm_32@hotmail.com>
2020-09-14 14:11:17 +0200

    -

--- Garage/Vehicle.cs
+++ Garage/Vehicle.cs
@@ -9,7 +9,30 @@ namespace Garage
     {
         #region properties ************************************************************************************
         public double Weight { get; set; }
-        public string RegistrationNumber { get; set; }
+        private string registrationNumber;
+
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+
+            set
+            {
+                char[] regNum = registrationNumber.ToCharArray();
+
+                if (Char.IsLetter(regNum[0]) && Char.IsLetter(regNum[1]) && Char.IsLetter(regNum[2]) &&
+                        Char.IsDigit(regNum[3]) && Char.IsDigit(regNum[4]) && Char.IsDigit(regNum[5]) )
+                {
+                    registrationNumber = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Registration number is wrong. Try again!")
+                }
+                
+            }
+        }
+
+        //public string RegistrationNumber { get; set; }
         public string Colour { get; set; }
         public int NumberOfWheels { get; set; }
         #endregion  
