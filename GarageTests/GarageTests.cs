482f05d6ea0852c7c6a8e3912fc3bc085086da84
stelstel <sthlm_32@hotmail.com>
2020-09-15 21:04:04 +0200

    Added class GarageTests

---
+++ GarageTests/GarageTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Garage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage.Tests
+{
+    [TestClass()]
+    public class GarageTests
+    {
+        static GarageHandler garageHandler = new GarageHandler();
+
+        [TestMethod()]
+        public void GarageTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void ParkVehicleTest()
+        {
+            /*
+            // Arrange
+            double expectedWeight = 250;
+            string expectedRegNum = "HJK812";
+            string expectedColour = "Red";
+
+            int expectedWheels = 4;
+            int expectedDoors = 2;
+            int expectedSize = 2;
+
+            // Act
+            Vehicle vehicle = new Motorcycle(250, "hjK812", "Red", 3, 950);
+            garageHandler.Garage.ParkVehicle(vehicle);
+            Vehicle vehicle2 = new Car(290, "HJK812", "Blue", 4, 2);
+            garageHandler.Garage.ParkVehicle(vehicle2);
+
+            double actualWeight = garageHandler.Garage.?????
+
+            // Assert
+            */
+        }
+
+        [TestMethod()]
+        public void UnparkVehicleTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void ListParkedVehiclesTest()
+        {
+            
+        }
+    }
+}
